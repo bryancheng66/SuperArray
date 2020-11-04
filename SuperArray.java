e7e46c508856429e8c7a4ddc3987a1ab434b6615
bryancheng66 <bryancheng66@gmail.com>
2020-11-03 22:11:29 -0500

    Change add to resize automatically if needed, and fix add's function

--- SuperArray.java
+++ SuperArray.java
@@ -12,14 +12,10 @@ public class SuperArray {
 	}
 
 	public boolean add(String element){
-		//Make an intermediary array with a length 1 greater, and copy all the elements of this.data into it, as well as the new element
-		String[] interArray = new String[this.data.length + 1];
-		for (int i = 0; i < this.data.length; i++){
-			interArray[i] = this.data[i];
-		}
-		interArray[interArray.length - 1] = element;
-
-		this.data = interArray;
+		if (size == this.data.length){
+			this.resize();
+		} 
+		this.data[size] = element;
 		this.size++;
 		return true;
 	}
