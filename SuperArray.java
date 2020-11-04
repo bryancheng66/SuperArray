31ee38d886ef4c708fda80cf3412deb747d2ca76
bryancheng66 <bryancheng66@gmail.com>
2020-11-03 20:41:06 -0500

    Add instance variables, constructor, and method size

---
+++ SuperArray.java
@@ -0,0 +1,13 @@
+public class SuperArray {
+	private String[] data;
+	private int size; //current size
+
+	public SuperArray(){
+		this.data = new String[0];
+		this.size = 0;
+	}
+	
+	public int size(){
+		return this.size;
+	}
+}
