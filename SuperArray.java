cd3ca043f97cf540a074fdd5a8c1e7eafb06617b
bryancheng66 <bryancheng66@gmail.com>
2020-11-03 21:00:33 -0500

    Add methods add and get

--- SuperArray.java
+++ SuperArray.java
@@ -10,4 +10,19 @@ public class SuperArray {
 	public int size(){
 		return this.size;
 	}
+
+	public boolean add(String element){
+		String[] interArray = new String[this.size + 1];
+		for (int i = 0; i < this.size; i++){
+			interArray[i] = this.data[i];
+		}
+		interArray[interArray.length - 1] = element;
+		this.data = interArray;
+		this.size++;
+		return true;
+	}
+
+	public String get(int index){
+		return this.data[index];
+	}
 }
---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+	public static void main(String[] Args){
+		SuperArray SA1 = new SuperArray();
+		SA1.add("hello");
+		SA1.add("maestro");
+		System.out.println(SA1.get(0));
+		System.out.println(SA1.get(1));
+	}
+}
