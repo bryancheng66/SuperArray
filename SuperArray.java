217fc9e60e4b6d2c435450ecb226a55d51e3dd9d
bryancheng66 <bryancheng66@gmail.com>
2020-11-03 21:32:58 -0500

    Fix size and some methods

--- SuperArray.java
+++ SuperArray.java
@@ -3,7 +3,7 @@ public class SuperArray {
 	private int size; //current size
 
 	public SuperArray(){
-		this.data = new String[0];
+		this.data = new String[10];
 		this.size = 0;
 	}
 	
@@ -12,11 +12,12 @@ public class SuperArray {
 	}
 
 	public boolean add(String element){
-		String[] interArray = new String[this.size + 1];
-		for (int i = 0; i < this.size; i++){
+		String[] interArray = new String[this.data.length + 1];
+		for (int i = 0; i < this.data.length; i++){
 			interArray[i] = this.data[i];
 		}
 		interArray[interArray.length - 1] = element;
+
 		this.data = interArray;
 		this.size++;
 		return true;
@@ -24,5 +25,13 @@ public class SuperArray {
 
 	public String get(int index){
 		return this.data[index];
+	}
+
+	public String set(int index, String element){
+		if (this.data[index] == null) size++;
+		
+		String replacee = this.data[index];
+		this.data[index] = element;
+		return replacee;
 	}
 }
--- Test.java
+++ Test.java
@@ -1,9 +1,12 @@
 public class Test {
 	public static void main(String[] Args){
+		//add() and get()
 		SuperArray SA1 = new SuperArray();
 		SA1.add("hello");
 		SA1.add("maestro");
 		System.out.println(SA1.get(0));
+		System.out.println(SA1.get(1));
+		System.out.println(SA1.set(1, "yello"));
 		System.out.println(SA1.get(1));
 	}
 }
